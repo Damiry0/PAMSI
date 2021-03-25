bfbe5482fb64b47844459938f4115feacb761e5a
Damiry0 <55887851+Damiry0@users.noreply.github.com>
2021-03-25 14:48:26 +0100

    Started Implementation of priority queue function

--- inc/priority_queue.h
+++ inc/priority_queue.h
@@ -1,33 +1,36 @@
-//
-// Created by damir on 3/24/2021.
-//
-
 #ifndef PAMSI_1_PRIORITY_QUEUE_H
 #define PAMSI_1_PRIORITY_QUEUE_H
 
-1 template <typename Comparable>
-2 class BinaryHeap
-3 {
-4 public:
-5 explicit BinaryHeap( int capacity = 100 );
-6 explicit BinaryHeap( const vector<Comparable> & items );
-7
-8 bool isEmpty( ) const;
-9 const Comparable & findMin( ) const;
-10
-11 void insert( const Comparable & x );
-12 void insert( Comparable && x );
-13 void deleteMin( );
-14 void deleteMin( Comparable & minItem );
-15 void makeEmpty( );
-16
-17 private:
-18 int currentSize; // Number of elements in heap
-19 vector<Comparable> array; // The heap array
-20
-21 void buildHeap( );
-22 void percolateDown( int hole );
-23 };
+template<typename Comparable>
+class BinaryHeap {
+public:
+    explicit BinaryHeap(int capacity = 100);
+
+    explicit BinaryHeap(const std::vector<Comparable> &items);
+
+    bool isEmpty() const;
+
+    const Comparable &findMin() const;
+
+    void insert(const Comparable &x);
+
+    void insert(Comparable &&x);
+
+    void deleteMin();
+
+    void deleteMin(Comparable &minItem);
+
+    void makeEmpty();
+
+private:
+    int currentSize; // Number of elements in heap
+    std::vector<Comparable> array; // The heap array
+
+    void buildHeap();
+
+    void percolateDown(int hole);
+};
+
 
 
 
--- src/priority_queue.cpp
+++ src/priority_queue.cpp
@@ -1,4 +1,64 @@
-//
-// Created by damir on 3/24/2021.
-//
+#include <vector>
+#include <iostream>
+#include "../inc/priority_queue.h"
+
+template<typename Comparable>
+BinaryHeap<Comparable>::BinaryHeap(int capacity) {
+
+}
+
+template<typename Comparable>
+BinaryHeap<Comparable>::BinaryHeap(const std::vector<Comparable> &items) {
+
+}
+
+template<typename Comparable>
+bool BinaryHeap<Comparable>::isEmpty() const {
+    return false;
+}
+
+template<typename Comparable>
+const Comparable &BinaryHeap<Comparable>::findMin() const {
+  //  return <#initializer#>;
+}
+
+template<typename Comparable>
+void BinaryHeap<Comparable>::insert(const Comparable &x) {
+
+}
+
+template<typename Comparable>
+void BinaryHeap<Comparable>::insert(Comparable &&x) {
+
+}
+
+template<typename Comparable>
+void BinaryHeap<Comparable>::deleteMin() {
+
+}
+
+template<typename Comparable>
+void BinaryHeap<Comparable>::deleteMin(Comparable &minItem) {
+
+}
+
+template<typename Comparable>
+void BinaryHeap<Comparable>::makeEmpty() {
+
+}
+
+template<typename Comparable>
+void BinaryHeap<Comparable>::buildHeap() {
+
+}
+
+template<typename Comparable>
+void BinaryHeap<Comparable>::percolateDown(int hole) {
+
+}
+
+
+
+
+
 
