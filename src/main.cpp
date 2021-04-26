77d36012ec5c8e0d6452a5e537a19eb0fcc979e0
Damiry0 <damian.rys0@gmail.com>
2021-04-26 14:17:34 +0200

    changes to priority queue layout, implementation of methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,92 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
-void SplitTxtIntoWords(std::string filename)
+#include "priority_queue.cpp"
+
+
+
+std::vector<std::string> split(const std::string & s, char c)
 {
-    std::ifstream file(filename);
-    std::string s;
-    while (std::getline(file, s))
+    std::vector<std::string> splitted;
+    std::string word;
+    for(char ch : s)
     {
-        // ...
+        if((ch == c) && (!word.empty()))
+        {
+            splitted.push_back(word);
+            word.clear();
+        }
+        else
+            word += ch;
     }
+    if(!word.empty())
+        splitted.push_back(word);
 
+    return splitted;
 }
 
-int main() {
-    for (int i = 0; i <10; ++i) {
 
+
+std::vector<std::string> SplitTxtIntoWords(std::string filename)
+{
+    std::ifstream in_s(filename);
+    std::vector <std::vector<std::string>> content;
+    std::vector <std::string> vec;
+    if(in_s)
+    {
+        std::string line;
+        
+        while(getline(in_s, line))
+        {
+            for(const std::string & str : split(line, ' '))
+                vec.push_back(str);
+            content.push_back(vec);
+            vec.clear();
+        }
+
+        in_s.close();
     }
-    return 0;
+    else
+        std::cout << "Could not open: " + filename << std::endl;
+
+    vector<string>str_vec;
+    for(const std::vector<std::string> & str_vec : content)
+    {
+        for(unsigned int i = 0; i < str_vec.size(); ++i)
+            std::cout << str_vec[i] <<std::endl;
+        std::cout << std::endl;
+    }
+    return str_vec;
+}
+
+
+
+int main() {
+   PriorityQueue<string> queue;
+   queue.enqueue("Ala ",1);
+   queue.enqueue("kota",7);
+   queue.enqueue("ma",5);
+   queue.enqueue("a kot ma ale",45);
+   std::vector<string>beka= SplitTxtIntoWords("../data/bitwa.txt");
+   for (int i = 0; i < beka.size(); i++)
+   {
+       std::cout<<beka[i]<<endl;
+   }
+   
+
+
+
+
+
+
+
+   queue.Print();
+
+
+
+  
+
+
 }
--- src/priority_queue.cpp
+++ src/priority_queue.cpp
@@ -1,47 +1,225 @@
+#include <cstdlib>
 #include <iostream>
-#include "../inc/priority_queue.h"
 
-template<typename T>
-PriorityQueue<T>::PriorityQueue(unsigned int size) {
-    _heap=NULL;
-    _currentSize=0;
-    _size=size;
+using namespace std;
+
+
+template<typename type>
+class PriorityQueue
+{
+	typedef struct Node
+	{
+		type field; //wartosc 
+		int key; // klucz
+		Node *nextNode; 
+	} *nodePointer;
+
+	nodePointer front;
+	nodePointer back;
+	int size=0;
+
+public:
+	void enqueue(const type& newElement, int priority);
+	void dequeue();
+	~PriorityQueue();
+	PriorityQueue();
+	PriorityQueue(const PriorityQueue &pque);
+
+
+	bool Empty();
+	void pushBack(const type& newElement, int priority);
+	void pushFront(const type& newElement, int priority);
+	void pushFirstNode(const type& newElement, int priority);
+	void pushInside(const type& newElement, int priority);
+	void Print();
+};
+
+
+template<typename type>
+PriorityQueue<type>::PriorityQueue()
+{
+	this->front = NULL;
+	this->back = NULL;
 }
 
-template<typename T>
-PriorityQueue<T>::~PriorityQueue() {
-    while (_heap) Remove();
+template<typename type>
+bool PriorityQueue<type>::Empty()
+{
+	if(this->front == NULL && this->back == NULL) return true;
+	else return false;
 }
 
-template<typename T>
-bool PriorityQueue<T>::Empty() const {
-    return _size==0 ? 1:0;
-}
-
-template<typename T>
-int PriorityQueue<T>::Size() const {
-    return _currentSize;
-}
-
-template<typename T>
-bool PriorityQueue<T>::Full() const {
-    return _currentSize==_size ? 1:0;
-}
-
-template<typename T>
-bool PriorityQueue<T>::Add(unsigned int key, T value) const {
-    if(this->Full) return false;
-
-}
-
-template<typename T>
-void PriorityQueue<T>::Print() const {
-
+template<typename type>
+void PriorityQueue<type>::Print()
+{
+	if(Empty())
+	{
+		cout << "Priority queue is empty " <<endl;
+	}
+	else
+	{
+		nodePointer temporary = back;
+		while(temporary!=NULL)
+		{
+			cout << "Field: " << temporary->field << "   Priority: " << temporary->key <<endl;
+			temporary = temporary->nextNode;
+		}
+		cout << endl;
+	}
 }
 
 
+template<typename type>
+void PriorityQueue<type>::pushFront(const type& newElement, int priority)
+{
+	nodePointer adder = new Node;           //
+	adder->key = priority;           		//inicjalizowanie obiektu node
+	adder->field = newElement;              //
+	adder->nextNode = NULL; 
+	
+	front->nextNode = adder;               
+	front = adder;
+	cout << "Enqueued element "<< newElement << " with priority " << priority << endl;
+}
+
+template<typename type>
+void PriorityQueue<type>::pushBack(const type& newElement, int priority)
+{
+	nodePointer adder = new Node;              //
+	adder->key = priority;             		   // creating new node to be added
+	adder->field = newElement;                //
+
+	adder->nextNode = back;                   // setting next node as beginner
+	back = adder;                             // reseting back
+	cout << "Enqueued element "<< newElement << " with priority " << priority << endl;
+}
+
+template<typename type>
+void PriorityQueue<type>::pushFirstNode(const type& newElement, int priority)
+{
+	nodePointer adder = new Node;
+	adder->key = priority;
+	adder->field = newElement;
+	adder->nextNode = NULL;
+	front = adder;                             //    
+	back = adder;                              // setting first node as back and front
+	cout << "Enqueued element "<< newElement << " with priority " << priority <<endl;
+}
 
 
+template<typename type>
+void PriorityQueue<type>::pushInside(const type& newElement, int priority)
+{                                   
+	nodePointer scroller = back;
+	nodePointer save = back;       // 'save' to remember last suitable scroller
+	while(scroller != front && scroller->key < priority)
+	{
+		save = scroller;                 // try to find a new place so that priority matches
+		scroller = scroller->nextNode;
+	}
+	nodePointer toPush = new Node;
+	toPush->key = priority;
+	toPush->field = newElement;
+	toPush->nextNode = scroller;
+	save->nextNode = toPush;
+	cout << "Enqueued element "<< newElement << " with priority " << priority << endl;
+}
 
 
+template<typename type>
+void PriorityQueue<type>::enqueue(const type& newElement, int priority)
+{
+	if(Empty())
+	{
+		pushFirstNode(newElement,priority);
+	}
+	else
+	{
+		if(front->key < priority)
+		{
+			pushFront(newElement,priority);
+		}
+		else
+		{
+			if(back->key > priority)
+			{
+				pushBack(newElement,priority);
+			}
+			else  
+			{
+				pushInside(newElement,priority);
+			}
+		}
+	}	
+
+}
+
+
+template<typename type>
+void PriorityQueue<type>::dequeue()
+{
+	if(Empty())
+	{
+		cout << "Priority queue is empty" <<endl;
+	}
+	else
+	{
+		if(back == front)
+		{
+			cout << "Dequeued element " << front->field << " with priority " << front->key << endl;
+			free(front);
+			front = NULL;
+			back = NULL;
+		}
+		else
+		{
+			nodePointer temporary = back;
+			nodePointer save = back;
+			while(temporary != front)
+			{
+				save = temporary;
+				temporary=temporary->nextNode;
+			}
+			cout << "Dequeued element " << front->field << " with priority " << front->key << endl;
+			front = save;
+			front->nextNode = NULL;
+			delete temporary;
+		}
+	}
+}
+
+template<typename type>
+PriorityQueue<type>::~PriorityQueue()
+{
+	while(back != NULL)
+	{
+		nodePointer temporary = back;
+		back = back->nextNode;
+		delete temporary;
+	}
+}
+
+
+template<typename type>
+PriorityQueue<type>::PriorityQueue(const PriorityQueue &pque)
+{
+
+	if(pque.front == NULL)
+	{
+		back = NULL;
+		front = NULL;
+	}
+	else
+	{
+		back = NULL;
+		front = NULL;
+		nodePointer temp = pque.back;
+
+		while(temp != NULL)
+		{
+			enqueue(temp->field,temp->key);
+			temp = temp->nextNode;
+		}
+	}
+}
 
